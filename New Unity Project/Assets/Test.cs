5be5777e19f2daba5d614e334fe6716ae4c1486b
Unknown <g1727039@st.t-kougei.ac.jp>
2018-05-21 17:36:38 +0900

    3回目のアセット

---
+++ New Unity Project/Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Helo,Worldをコンソールウィンドウに表示する
+        Debug.Log("Hello,World");
+        float height1 = 160.5f;
+        float height2;
+        height2 = height1;
+        Debug.Log(height2);
+        int answer;
+        answer = 1 + 2;
+        Debug.Log(answer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
